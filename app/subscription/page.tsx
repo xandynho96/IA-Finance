39b63fd26b1abfc8a8a366df61eac36c3e4e11ab
Alexandre Ferreira <x.ande96@hotmail.com>
2025-01-12 11:15:44 -0300

    chore: add shadcn setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const Subscription = () => {
+  return <h1>subscription page</h1>;
+};
+
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const Transactions = () => {
+  return <h1>Transactions page</h1>;
+};
+
+export default Transactions;
